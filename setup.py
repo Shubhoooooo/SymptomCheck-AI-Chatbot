5b4154eb13cb908007cfdf3211714adceb6dbe37
Shubhodeep8 <shubhodeep.ghatu@tigeranalytics.com>
2025-11-05 02:37:30 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="SymptomCheck-AiChatbot",
+    version="0.1.0",
+    author="Shubhodeep Ghatuary",
+    author_email="shubhodeepghatuary2004@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
